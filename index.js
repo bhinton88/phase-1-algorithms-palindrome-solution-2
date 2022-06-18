af313a80a436f82fc66e2982139234fea6d6a012
Bobby Hinton <bob.g.hint@gmail.com>
2022-06-18 11:54:20 -0500

    commit tree 6baec5c71e2b1102a667023db2ef79eb2be8c31f

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+for(let i = 0; i < word.length/2; i++) {
+  const j = word.length - 1 - i
+  const startChar = word[i]
+  const endChar = word[j]
+  if(startChar !== endChar) return false
+}
+return true
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+// r a c e c a r
+// 0 1 2 3 4 5 6
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
